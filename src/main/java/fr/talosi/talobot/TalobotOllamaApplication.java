f3241ead03ae3adeaf290ef7f6ada2477173ff3e
Noe <noe.pamula@gmail.com>
2025-02-21 20:23:10 +0100

    :tada: init project

---
+++ src/main/java/fr/talosi/talobot/TalobotOllamaApplication.java
@@ -0,0 +1,13 @@
+package fr.talosi.talobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalobotOllamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalobotOllamaApplication.class, args);
+	}
+
+}
